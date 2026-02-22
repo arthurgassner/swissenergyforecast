4ea823036431bb96295930748b2e0d526d7e3a61
Arthur Gassner <arth.gassner@gmail.com>
2026-02-22 15:24:02 +0100

    test: adapt tests

--- backend/tests/app/schemas/forecast.py
+++ backend/tests/app/schemas/forecast.py
@@ -25,3 +25,27 @@ def test_compute_mapes__perfect_prediction():
     # then
     assert len(mapes) == len(timedelta_strs)  # 4 timedelta_strs where given
     assert all(e.score == 0.0 for e in mapes) # The predictions are always without error
+
+def test_compute_mapes__mape_changes():
+    """Check that the MAPE changes with the increased deltatimes."""
+
+    # Given a df of the expected format
+    index = pd.DatetimeIndex(
+            [
+                pd.Timestamp("20240101 23:45", tz="Europe/Zurich"),
+                pd.Timestamp("20240102 23:45", tz="Europe/Zurich"),
+                pd.Timestamp("20240103 23:45", tz="Europe/Zurich"),
+            ]
+        )
+    y = pd.Series([101.0, 202.0, 303.0], index=index)
+    yhat = pd.Series([101.0, 101.0, 303.0], index=index)
+    timedelta_strs = ["1h", "24h", "2D"]
+
+    # when
+    mapes = MAPE.compute_mapes(y, yhat, timedelta_strs=timedelta_strs)
+
+    # then
+    assert len(mapes) == len(timedelta_strs)  # As many rows as given timedeltas
+    assert np.isclose(mapes[0].score, 0.0, atol=0.1)
+    assert np.isclose(mapes[1].score, 25.0, atol=0.1)
+    assert np.isclose(mapes[2].score, 16.6, atol=0.1)
--- backend/tests/app/services/test_performance_measure_service.py
+++ backend/tests/app/services/test_performance_measure_service.py
@@ -4,38 +4,6 @@ import numpy as np
 import pandas as pd
 
 
-def test_compute_mape__perfect_prediction():
-    """Check that the MAPE of a perfect prediction is 0.0."""
-
-    # Given a df of the expected format
-    df = pd.DataFrame(
-        {
-            "Forecasted Load": [101.0, 202.0, 303.0],
-            "Actual Load": [101.0, 202.0, 303.0],
-        },
-        index=pd.DatetimeIndex(
-            [
-                pd.Timestamp("20240101 23:45", tz="Europe/Zurich"),
-                pd.Timestamp("20240102 23:45", tz="Europe/Zurich"),
-                pd.Timestamp("20240103 23:45", tz="Europe/Zurich"),
-            ]
-        ),
-    )
-
-    # when
-    timedeltas = [timedelta(days=3 + i) for i in range(5)]
-    mape_df = performance_measure_service.compute_mape(
-        "Actual Load",
-        "Forecasted Load",
-        data=df,
-        timedeltas=timedeltas,
-    )
-
-    # then
-    assert len(mape_df) == len(timedeltas)  # 5 timedeltas where given
-    assert (mape_df.mape == 0.0).all()  # The predictions are always without error
-
-
 def test_compute_mape__mape_changes():
     """Check that the MAPE changes with the increased deltatimes."""
 
