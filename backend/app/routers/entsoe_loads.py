c2285f5f8bf5d5b9023bf9987c62c411d4fb401d
Arthur Gassner <arth.gassner@gmail.com>
2026-02-22 13:44:27 +0100

    feat: add GET /loads

--- backend/app/routers/entsoe_loads.py
+++ backend/app/routers/entsoe_loads.py
@@ -1,39 +1,38 @@
+from datetime import timedelta
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
+
+from fastapi.responses import ORJSONResponse
 import pandas as pd
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query
 from loguru import logger
 
+from app.clients.db_client import DBCLient
 from app.core.config import get_settings
-from app.schemas.entsoe_loads_fetch_latest import (
-    EntsoeLoadsFetchLatestRequest,
-    EntsoeLoadsFetchLatestResponse,
+from app.core.dependencies import get_db_client
+from app.schemas.entsoe_loads import (
+    ENTSOELoads,
 )
 
 router = APIRouter()
 
 
-@router.post("/entsoe-loads/fetch/latest")
-async def post_entsoe_loads_fetch_latest(request: EntsoeLoadsFetchLatestRequest) -> EntsoeLoadsFetchLatestResponse:
+@router.get("/loads", response_class=ORJSONResponse)
+async def get_loads(
+    days: int = Query(0, ge=0, description="How many days to look back"),
+    hours: int = Query(0, ge=0, description="How many hours to look back"),
+    db_client: DBCLient = Depends(get_db_client)
+    ) -> ENTSOELoads:
+
     # Load past loads
-    silver_df = pd.read_pickle(get_settings().SILVER_DF_FILEPATH)
+    silver_df = await db_client.load_silver()
+    if silver_df is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No ENTSO-E loads found in the database")
 
     # Figure out till when the records should be sent
     end_ts = silver_df.index.max()
-    cutoff_ts = end_ts - request.delta_time
+    cutoff_ts = end_ts - timedelta(days=days, hours=hours)
 
     # Only keep the data till
     silver_df = silver_df[silver_df.index > cutoff_ts]
 
-    response = EntsoeLoadsFetchLatestResponse(
-        timestamps=silver_df.index.tolist(),
-        day_later_loads=silver_df["24h_later_load"].astype(float).fillna("NaN").tolist(),
-        day_later_forecasts=silver_df["24h_later_forecast"].astype(float).fillna("NaN").tolist(),
-    )
-
-    if len(response.timestamps):
-        logger.info(f"Ready to send back: {len(response.timestamps)} timestamps between {cutoff_ts} -> {end_ts}")
-    else:
-        logger.warning(
-            f"Ready to send empty dict: {len(response.timestamps)} timestamps between {cutoff_ts} -> {end_ts}"
-        )
-
-    return response
+    return ENTSOELoads(timestamps=silver_df.index.tolist(), day_later_loads=silver_df["24h_later_load"].astype(float).tolist())
