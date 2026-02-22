11ab1b1cac1f48c64706f4e6d1a5aac390b5499d
Arthur Gassner <arth.gassner@gmail.com>
2026-02-22 18:51:15 +0100

    chore: improve logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from app.routers.forecast import router as forecast_router
 from app.routers.loads import router as entsoe_loads_router
 from fastapi import FastAPI
 from fastapi import Request
+from fastapi import status
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
@@ -26,9 +27,13 @@ async def middleware(request: Request, call_next):
     response = await call_next(request)
 
     elapsed_time_s = time.perf_counter() - start_time_s
-    logger.info(
-        f"from {request.client.host}:{request.client.port} | {request.method} {request.url}: {response.status_code} {elapsed_time_s * 1e3:.0f}ms"
-    )
+    log_str = f"from {request.client.host}:{request.client.port} | {request.method} {request.url}: {response.status_code} {elapsed_time_s * 1e3:.0f}ms"
+    if response.status_code < status.HTTP_400_BAD_REQUEST:
+        logger.info(log_str)
+    elif response.status_code < status.HTTP_500_INTERNAL_SERVER_ERROR:
+        logger.warning(log_str)
+    else:
+        logger.error(log_str)
 
     response.headers["X-Process-Time"] = str(elapsed_time_s)
 
