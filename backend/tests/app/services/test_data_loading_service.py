9cef79435a25e96a872d81608a19c43b8e7eb65e
Arthur Gassner <arth.gassner@gmail.com>
2026-02-22 15:26:59 +0100

    test: adapt tests

--- backend/tests/app/services/test_data_loading_service.py
+++ backend/tests/app/services/test_data_loading_service.py
@@ -9,38 +9,6 @@ from app.core.config import get_settings
 from app.services import data_loading_service
 
 
-def test__split_yearly():
-    """Ensure time intervals are split as expected."""
-
-    # Given 
-    start_ts = pd.Timestamp("2014-01-01 00:00", tz="Europe/Zurich")
-    end_ts = pd.Timestamp("2016-01-02 00:00", tz="Europe/Zurich")
-    
-    # When
-    start_end_timestamps = data_loading_service._split_yearly(start_ts=start_ts, end_ts=end_ts)
-
-    # Then
-    assert len(start_end_timestamps) == 3
-    assert start_end_timestamps[0][0] == start_ts
-    assert start_end_timestamps[-1][1] == end_ts
-
-    first_split_timedelta = (start_end_timestamps[0][1] - start_end_timestamps[0][0])
-    assert first_split_timedelta == pd.Timedelta(days=365) # Since the start_ts is the earliest ts in 2014
-
-    last_split_timedelta = (start_end_timestamps[-1][1] - start_end_timestamps[-1][0])
-    assert last_split_timedelta == pd.Timedelta(days=1) # Since end_ts is 1 day after the start of the year
-
-def test__split_yearly__end_before_start():
-    """Ensure time intervals raises a ValueError when the end is before the start"""
-
-    # Given 
-    start_ts = pd.Timestamp("2014-01-02 00:00", tz="Europe/Zurich")
-    end_ts = pd.Timestamp("2014-01-01 00:00", tz="Europe/Zurich")
-    
-    # When - Then
-    with pytest.raises(ValueError):
-        data_loading_service._split_yearly(start_ts=start_ts, end_ts=end_ts)
-
 
 def test__query_load_and_forecast__future_ts():
     """Querying the ENTSO-E API with a timestamp 48h in the future should result in an empty df."""
